c49e3a517c34e41df514a592c40ec6cd61cd8a97
JagamohanPanigrahi <142359288+JagamohanPanigrahi@users.noreply.github.com>
2023-12-02 15:49:35 +0530

    Update Ami-idCheckout.sh

--- Ami-idCheckout.sh
+++ Ami-idCheckout.sh
@@ -4,12 +4,5 @@ check_ami_status() {
 
     echo "Checking status of AMI $ami_id in region $region..."
     status=$(aws ec2 describe-images --image-ids $ami_id --region $region --query 'Images[0].State' --output text 2>&1)
-
-    if [[ $status == "available" ]]; then
-        echo "AMI $ami_id is available in region $region."
-        return 0
-    else
-        echo "AMI $ami_id is not available in region $region or does not exist."
-        return 1
-    fi
+    export status1=$status
 }
