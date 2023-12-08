a4c37bf0fc80bde5f1ed90fe605d893bca4634bf
JagamohanPanigrahi <142359288+JagamohanPanigrahi@users.noreply.github.com>
2023-12-08 08:05:30 +0530

    Update Amideletion.sh

--- Amideletion.sh
+++ Amideletion.sh
@@ -8,7 +8,7 @@ check_ami_status() {
     echo "Checking status of AMI $ami_id in region $region..."
     status=$(aws ec2 describe-images --image-ids $ami_id --region $region --query 'Images[0].State' --output text 2>&1)
 
-    if [[ $status == "available" ]]; then
+    if [[ $status == "Available" ]]; then
         echo "AMI $ami_id is available in region $region."
         return 0
     else
