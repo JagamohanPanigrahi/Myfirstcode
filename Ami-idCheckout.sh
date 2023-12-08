54f055b859f835cdbfd189d1d5ff66338e20c336
JagamohanPanigrahi <142359288+JagamohanPanigrahi@users.noreply.github.com>
2023-12-08 07:46:06 +0530

    Update Ami-idCheckout.sh

--- Ami-idCheckout.sh
+++ Ami-idCheckout.sh
@@ -4,7 +4,7 @@ check_ami_status() {
     local ami_id=$1
     local region=$2
 
-check_ami_status() {
+
     echo "Checking status of AMI $ami_id in region $region..."
     status=$(aws ec2 describe-images --image-ids $ami_id --region $region --query 'Images[0].State' --output text 2>&1)
     echo "Status of AMI $ami_id: $status"
