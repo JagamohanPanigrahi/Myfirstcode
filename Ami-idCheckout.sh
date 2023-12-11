b151385d0e49d7e0581225b5a8945fdecffdb110
JagamohanPanigrahi <142359288+JagamohanPanigrahi@users.noreply.github.com>
2023-12-11 22:24:22 +0530

    Update Ami-idCheckout.sh

--- Ami-idCheckout.sh
+++ Ami-idCheckout.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Input parameters
+
 AWS_AMI_ID=$1
 AWS_REGION=$2
 
-# Validate input parameters
+
 if [ -z "$AWS_AMI_ID" ]; then
     echo "Error: AMI ID is not provided."
     exit 1
@@ -15,10 +15,10 @@ if [ -z "$AWS_REGION" ]; then
     exit 1
 fi
 
-# AWS CLI command to check the status of the specified AMI
+
 status=$(aws ec2 describe-images --image-ids "$AWS_AMI_ID" --region "$AWS_REGION" --query 'Images[0].State' --output text 2>&1)
 
-# Check if the describe-images command was successful
+
 if [ $? -eq 0 ]; then
     echo "Status of AMI $AWS_AMI_ID in region $AWS_REGION: $status"
 else
