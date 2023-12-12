09f8b12dba44974e5563da9cf30a9e73e1ee698f
JagamohanPanigrahi <142359288+JagamohanPanigrahi@users.noreply.github.com>
2023-12-13 01:40:51 +0530

    Update BulkAMI-idCheckout.sh

--- BulkAMI-idCheckout.sh
+++ BulkAMI-idCheckout.sh
@@ -20,11 +20,17 @@ while IFS=, read -r AWS_AMI_ID || [[ -n "$AWS_AMI_ID" ]]; do
         continue  # Skip empty lines
     fi
 
+    # Check if the AMI ID is well-formed before making the API call
+    if [[ ! "$AWS_AMI_ID" =~ ^ami-[a-fA-F0-9]{8,}$ ]]; then
+        echo "Error: Invalid AMI ID format for $AWS_AMI_ID. Skipping..."
+        continue
+    fi
+
     status=$(aws ec2 describe-images --image-ids "$AWS_AMI_ID" --region "$AWS_REGION" --query 'Images[0].State' --output text 2>&1)
 
-    if [[ "$status" == "Available" ]]; then
+    if [ $? -eq 0 ]; then
         echo "Status of AMI $AWS_AMI_ID in region $AWS_REGION: $status"
     else
-        echo "Error describing AMI $AWS_AMI_ID in region $AWS_REGION: $status"
+        echo "Error describing AMI $AWS_AMI_ID in region $AWS_REGION. AWS CLI error message: $status"
     fi
 done < "$CSV_FILE"
