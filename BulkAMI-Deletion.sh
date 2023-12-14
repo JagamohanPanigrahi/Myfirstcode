f8ed3511c9465e16d24a36bbca7abe28c512763d
JagamohanPanigrahi <142359288+JagamohanPanigrahi@users.noreply.github.com>
2023-12-14 23:15:06 +0530

    Update BulkAMI-Deletion.sh

--- BulkAMI-Deletion.sh
+++ BulkAMI-Deletion.sh
@@ -6,7 +6,7 @@ AWS_REGION=$2
 shopt -s nocasematch
 
 while IFS=, read -r ami_id || [[ -n "$ami_id" ]]; do
-    
+   
     if [ -z "$ami_id" ]; then
         continue
     fi
@@ -16,13 +16,13 @@ while IFS=, read -r ami_id || [[ -n "$ami_id" ]]; do
         continue
     fi
 
-echo "Deleting AMI $AWS_AMI_ID in region $AWS_REGION..."
-status=$(aws ec2 deregister-image --image-ids "$ami-id" --region "$AWS_REGION" --query 'Images[0].State' --output text 2>&1)
+    echo "Deleting AMI $ami_id in region $AWS_REGION..."
+    status=$(aws ec2 deregister-image --image-ids "$ami_id" --region "$AWS_REGION" --query 'Images[0].State' --output text 2>&1)
 
-if [ $? -eq 0 ]; then
-    echo "AMIs $CSV_FILE has been deleted in region $AWS_REGION."
-else
-    echo "Error deleting AMI $CSV_FILE in region $AWS_REGION."
-    exit 1
-fi
+    if [ $? -eq 0 ]; then
+        echo "AMI $ami_id has been deleted in region $AWS_REGION."
+    else
+        echo "Error deleting AMI $ami_id in region $AWS_REGION."
+        exit 1
+    fi
 done < "$CSV_FILE"
