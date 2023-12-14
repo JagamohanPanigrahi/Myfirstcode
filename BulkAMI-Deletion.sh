1298ad75db7d2ea41f4371bec24ea36060f80b99
JagamohanPanigrahi <142359288+JagamohanPanigrahi@users.noreply.github.com>
2023-12-14 22:54:53 +0530

    Create BulkAMI-Deletion.sh

---
+++ BulkAMI-Deletion.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CSV_FILE=$1
+AWS_REGION=$2
+
+shopt -s nocasematch
+
+while IFS=, read -r ami_id || [[ -n "$ami_id" ]]; do
+    ami_id=$(trim "$ami_id")
+
+    if [ -z "$ami_id" ]; then
+        continue
+    fi
+
+    if [[ ! "$ami_id" =~ ^ami-[a-fA-F0-9]{8,}$ ]]; then
+        echo "Error: Invalid AMI ID format for $ami_id. Skipping...this job"
+        continue
+    fi
+
+echo "Deleting AMI $AWS_AMI_ID in region $AWS_REGION..."
+status=$(aws ec2 deregister-image --image-ids "$ami-id" --region "$AWS_REGION" --query 'Images[0].State' --output text 2>&1)
+
+if [ $? -eq 0 ]; then
+    echo "AMIs $CSV_FILE has been deleted in region $AWS_REGION."
+else
+    echo "Error deleting AMI $CSV_FILE in region $AWS_REGION."
+    exit 1
+fi
