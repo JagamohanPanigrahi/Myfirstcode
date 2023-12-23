849699835f88fc6be35faeadf3a7daaccd121423
JagamohanPanigrahi <142359288+JagamohanPanigrahi@users.noreply.github.com>
2023-12-23 23:37:42 +0530

    Update BulkAMI-idCheckout.sh

--- BulkAMI-idCheckout.sh
+++ BulkAMI-idCheckout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CSV_FILE="$1"
-REGIONS=("us-east-1" "us-west-2" "eu-west-1 " "eu-central-1" "ap-southeast-1" "ap-southeast-2")  # Add your desired AWS regions to this array
+AWS_REGION="$2"
 
 trim() {
     echo "$1" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
@@ -9,6 +9,11 @@ trim() {
 
 if [ -z "$CSV_FILE" ]; then
     echo "Error: CSV file path is not provided."
+    exit 1
+fi
+
+if [ -z "$AWS_REGION" ]; then
+    echo "Error: AWS region is not provided."
     exit 1
 fi
 
@@ -25,25 +30,17 @@ while IFS= read -r ami_id || [[ -n "$ami_id" ]]; do
         echo "Error: Invalid AMI ID format for $ami_id. Skipping...this job"
         exit 1
     else
-        success=false
+        result=$(aws ec2 describe-images --image-ids "$ami_id" --region "$AWS_REGION" \
+                 --query 'Images[0].[State,Name]' --output text 2>&1)
 
-        for region in "${REGIONS[@]}"; do
-            result=$(aws ec2 describe-images --image-ids "$ami_id" --region "$region" \
-                     --query 'Images[0].[State,Name]' --output text 2>&1)
+        status=$(echo "$result" | awk '{print $1}')
+        name=$(echo "$result" | awk '{print $2}')
 
-            status=$(echo "$result" | awk '{print $1}')
-            name=$(echo "$result" | awk '{print $2}')
-
-            if [ "$status" == "available" ]; then
-                echo "Status of AMI $ami_id in region $region: $status"
-                echo "Name of AMI $ami_id: $name"
-                success=true
-                break  # Break out of the loop if AMI is found in any region
-            fi
-        done
-
-        if [ "$success" != true ]; then
-            echo "Error describing AMI $ami_id in all regions. AMI State is: $result"
+        if [ "$status" == "available" ]; then
+            echo "Status of AMI $ami_id in region $AWS_REGION: $status"
+            echo "Name of AMI $ami_id: $name"
+        else
+            echo "Error describing AMI $ami_id in region $AWS_REGION. AMI State is: $status"
             exit 1
         fi
     fi
